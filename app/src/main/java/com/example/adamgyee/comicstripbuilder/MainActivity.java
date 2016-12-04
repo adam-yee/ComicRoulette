dbe0f3d70c347d65c122df5788d1d508509c0b75
Adam Yee <adamy@andrew.cmu.edu>
2016-12-03 20:42:58 -0500

    moved num_artists spinner to popup

--- app/src/main/java/com/example/adamgyee/comicstripbuilder/MainActivity.java
+++ app/src/main/java/com/example/adamgyee/comicstripbuilder/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.adamgyee.comicstripbuilder;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import com.simplify.ink.InkView;
@@ -42,13 +50,101 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Spinner numArtistSpinner = (Spinner) findViewById(R.id.num_artists_spinner);
-        ArrayAdapter<CharSequence> spinner_adapter = ArrayAdapter.createFromResource(this,
-                R.array.num_artists_array, android.R.layout.simple_spinner_item);
-        spinner_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        numArtistSpinner.setAdapter(spinner_adapter);
-
         // Get permissions
+        getExternalStoragePermission();
+
+        mNewStrip = (ImageButton) findViewById(R.id.new_strip_btn);
+        mNewStrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Prompt spinner and send to drawactivity
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setImageResource(R.drawable.pass_image1);
+                loadPhoto(imageView, 100, 100);
+
+            }
+        });
+
+        mOnlineRoulette = (Button) findViewById(R.id.online_btn);
+        mOnlineRoulette.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO: get internet permissions
+                getInternetPermissions();
+
+                Intent nextIntent = new Intent(MainActivity.this, OnlineDrawActivity.class);
+                MainActivity.this.startActivity(nextIntent);
+                MainActivity.this.finish();
+            }
+        });
+
+    }
+
+    private void loadPhoto(ImageView imageView, int width, int height) {
+
+        AlertDialog.Builder imageDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        final View layout = inflater.inflate(R.layout.popup_num_artists,
+                (ViewGroup) findViewById(R.id.layout_root));
+
+        imageDialog.setView(layout);
+        imageDialog.setPositiveButton(getString(R.string.action_done), new DialogInterface.OnClickListener(){
+
+            public void onClick(DialogInterface dialog, int which) {
+
+                //numArtistSpinner.getSelectedItem().toString();
+                RadioGroup radioGroup = (RadioGroup) layout.findViewById(R.id.num_artsts_rdio);
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                RadioButton radioButton = (RadioButton) layout.findViewById(selectedId);
+
+                int numArtistsInt = Integer.parseInt(radioButton.getText().toString());
+                dialog.dismiss();
+                Intent nextIntent = new Intent(MainActivity.this, DrawActivity.class);
+                nextIntent.putExtra("numArtists", numArtistsInt);
+                MainActivity.this.startActivity(nextIntent);
+                MainActivity.this.finish();
+            }
+        });
+
+        imageDialog.create();
+        imageDialog.show();
+    }
+
+    private void getInternetPermissions(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.INTERNET)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.INTERNET},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    private void getExternalStoragePermission(){
         if (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -72,77 +168,5 @@ public class MainActivity extends AppCompatActivity {
                 // result of the request.
             }
         }
-
-        mNewStrip = (ImageButton) findViewById(R.id.new_strip_btn);
-        mNewStrip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // Grab the number of artists from spinner
-                // TODO: make this not dumb
-                int numArtistsInt = 3;
-                String numArtists = numArtistSpinner.getSelectedItem().toString();
-                switch (numArtists){
-                    case "3":
-                        numArtistsInt = 3;
-                        break;
-                    case "4":
-                        numArtistsInt = 4;
-                        break;
-                    case "5":
-                        numArtistsInt = 5;
-                        break;
-                    default:
-                        // Shouldn't ever be here
-                }
-
-                // Send numArtists to next intent
-                Intent nextIntent = new Intent(MainActivity.this, DrawActivity.class);
-                nextIntent.putExtra("numArtists", numArtistsInt);
-                MainActivity.this.startActivity(nextIntent);
-                MainActivity.this.finish();
-            }
-        });
-
-        mOnlineRoulette = (Button) findViewById(R.id.online_btn);
-        mOnlineRoulette.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                // TODO: get internet permissions
-
-                // Here, thisActivity is the current activity
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.INTERNET)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-                    // Should we show an explanation?
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                            Manifest.permission.INTERNET)) {
-
-                        // Show an explanation to the user *asynchronously* -- don't block
-                        // this thread waiting for the user's response! After the user
-                        // sees the explanation, try again to request the permission.
-
-                    } else {
-
-                        // No explanation needed, we can request the permission.
-
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.INTERNET},
-                                1);
-
-                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                        // app-defined int constant. The callback method gets the
-                        // result of the request.
-                    }
-                }
-
-                Intent nextIntent = new Intent(MainActivity.this, OnlineDrawActivity.class);
-                MainActivity.this.startActivity(nextIntent);
-                MainActivity.this.finish();
-            }
-        });
-
     }
 }
